8bbce912adc452bbed49099a5d9546a5953f8dd8
Hanna Krzh <anutkakrz@gmail.com>
2024-02-20 09:58:41 +0100

    1

--- src/main/java/org/example/java1902/HelloWorld.java
+++ src/main/java/org/example/java1902/HelloWorld.java
@@ -3,7 +3,6 @@ package org.example.java1902;
 public class HelloWorld {
     public static void main(String[] args)
     {
-
         System.out.println("Hello");
         System.out.println(25);
     }
