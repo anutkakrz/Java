48313a05a82922a0bbf84e79ddbdb34db2f49a5d
Hanna Krzh <anutkakrz@gmail.com>
2024-03-12 20:32:18 +0100

    1203

---
+++ src/main/java/org/example/java0103/Java0103.java
@@ -0,0 +1,34 @@
+package org.example.java0103;
+
+import java.util.Arrays;
+
+public class Java0103 {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        int array1[] = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
+
+        System.out.println(Arrays.toString(array1));
+
+        int[] array2 = {1, 2, 3};
+        int array3[] = {1, 2, 3};
+//
+        int[] array4 = {1, 3, 6} ;
+        System.out.println(Arrays.toString(array4));
+
+
+//        Написать код по определению значения и индекса минимального по модулю
+//        числа в массиве. Массив элементов инициализировать положительными
+//        и отрицательными числами 10, -5, 8, -12, 3, 6, 0.
+
+    }
+
+
+}
+
+
